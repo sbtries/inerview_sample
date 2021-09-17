fed76234301ed64858e31958221a618cd79fa1e5
sbtries <sarahbtracy17@gmail.com>
2021-09-17 16:25:52 -0400

    added scaffold for groups: wow this creates a lot of stuff

--- rails_meetups/app/models/group.rb
+++ rails_meetups/app/models/group.rb
@@ -1,16 +1,2 @@
 class Group < ApplicationRecord
-
-  validates :title, presence: true
-
-  belongs_to :user,
-  primary_key: :id,
-  foreign_key: :organizer_id,
-  class_name: :User
-  
-  
-  has_many :memberships,
-  foreign_key: :group_id,
-  class_name: :GroupMember
-  
-  has_many :members, through: :memberships, source: :member
-  end
+end
